8526da56f4e254495ed620f571c35307c5259acd
Aryan Sinha <arison.aryan@gmail.com>
2022-07-21 12:42:23 +0530

    n

--- linkedlist/LinkedList.cpp
+++ linkedlist/LinkedList.cpp
@@ -7,19 +7,19 @@ struct Node
     struct Node *next;
 } *first = NULL;
 
-void create(int a[], int n)
+void create(int A[], int n)
 {
     int i;
     struct Node *t, *last;
-    first = new Node;
-    first->data = a[0];
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
     first->next = NULL;
     last = first;
 
-    for (int i = 1; i < n; i++)
+    for (i = 1; i < n; i++)
     {
-        t = new Node;
-        t->data = a[i];
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
         t->next = NULL;
         last->next = t;
         last = t;
@@ -113,6 +113,7 @@ int RMax(struct Node *p) // recursive method
     {
         x = RMax(p->next);
     }
+    return x;
 }
 
 struct Node *LSearch2(struct Node *p, int key)
@@ -125,10 +126,12 @@ struct Node *LSearch2(struct Node *p, int key)
             q->next = p->next;
             p->next = first;
             first = p;
+            return p;
         }
         q = p;
         p = p->next;
     }
+    return NULL;
 }
 
 struct Node *LSearch(struct Node *p, int key)
@@ -152,58 +155,226 @@ struct Node *RSearch(struct Node *p, int key)
         return RSearch(p->next, key);
 }
 
-void insert(int pos, int x)
+void insert(struct Node *p, int index, int x)
 {
-    Node *t, *p;
-    if (pos == 0)
+    struct Node *t;
+
+    if (index < 0 || index > count(p))
+        return;
+    t = new Node;
+    t->data = x;
+
+    if (index == 0)
     {
-        t = new Node;
-        t->data = x;
         t->next = first;
         first = t;
     }
     else
     {
-        p = first;
-        for (int i = 0; i < pos; i++)
+        for (int i = 0; i < index - 1; i++)
             p = p->next;
-        if (p)
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
+void SortedInsert(struct Node *p, int x)
+{
+    struct Node *t, *q = NULL;
+    t = new Node;
+    t->data = x;
+    t->next = NULL;
+    if (first == NULL)
+        first = t;
+    else
+    {
+        while (p && p->data < x)
         {
-            t = new Node;
-            t->data = x;
-            t->next = p->next;
-            p->next = t;
+            q = p;
+            p = p->next;
         }
+        if (p == first)
+        {
+            t->next = first;
+            first = t;
+        }
+        else
+        {
+            t->next = q->next;
+            q->next = t;
+        }
+    }
+}
+
+int Delete(struct Node *p, int index)
+{
+    struct Node *q;
+    int x = -1;
+
+    if (index < 1 || index > count(p))
+        return -1;
+    if (index == 1)
+    {
+        q = first;
+        x = first->data;
+        first = first->next;
+        delete q;
+        return x;
+    }
+    else
+    {
+        p = first;
+        q = NULL;
+        for (int i = 0; i < index - 1; ++i)
+        {
+            q = p;
+            p = p->next;
+        }
+        q->next = p->next;
+        x = p->data;
+        delete p;
+    }
+    return x;
+}
+
+int isSorted(struct Node *p)
+{
+    int x = INT_MIN;
+    p = first;
+    while (p != NULL)
+    {
+        if (p->data < x)
+            return false;
+        x = p->data;
+        p = p->next;
+    }
+    return true;
+}
+
+void RemoveDuplicate(struct Node *p)
+{
+    Node *q = p->next;
+    while (q != NULL)
+    {
+        if (p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            delete q;
+            q = p->next;
+        }
+    }
+}
+
+void Reverse1(struct Node *p)
+{
+    int *A, i = 0;
+    struct Node *q = p;
+
+    A = new int[count(p)];
+
+    while (q != NULL)
+    {
+        A[i++] = q->data;
+        q = q->next;
+    }
+    q = p;
+    i--;
+    while (p != NULL)
+    {
+        p->data = A[i--];
+        p = p->next;
+    }
+}
+
+void Reverse2(struct Node *p)
+{
+    struct Node *q = NULL;
+    struct Node *r = NULL;
+    while (p != NULL)
+    {
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = p;
+    }
+}
+
+void Reverse3(struct Node *q, struct Node *p)
+{
+    if (p != NULL)
+    {
+        Reverse3(p, p->next);
+        p->next = q;
+    }
+    else
+    {
+        first = q;
     }
 }
 
 int main()
 {
-    int a[] = {3, 4, 5, 6, 7, 8, 12, 56};
 
-    create(a, 8);
+    // create(a, 8);
+    //  display(first);
+
+    // cout << "Recursive display: " << endl;
+    // Rdisplay(first);
+    // cout << endl;
+
+    // cout << "Reverse recursive display: " << endl;
+    // RRdisplay(first);
+    // cout << endl;
+
+    // cout << "Number of nodes: " << Rcount(first) << endl;
+
+    // cout << "Sum of all nodes: " << Rsum(first) << endl;
+
+    // cout << "Max number is: " << Max(first) << endl;
+
+    // struct Node *temp;
+    // temp = LSearch(first, 25);
+    // if (temp)
+    //     cout << "Key is found " << temp->data << endl;
+    // else
+    //     cout << "Key is not found" << endl;
+
+    // insert(first, 8, 10);
+
+    // display(first);
+
+    int A[] = {10, 10, 10, 20, 30, 30, 40, 50};
+    create(A, 5);
+
+    // Inserted in a sorted linked list
+    SortedInsert(first, 25);
     display(first);
 
-    cout << "Recursive display: " << endl;
-    Rdisplay(first);
-    cout << endl;
+    // Deleting element
+    cout << "Deleted element: " << Delete(first, 1) << endl;
+    display(first);
 
-    cout << "Reverse recursive display: " << endl;
-    RRdisplay(first);
-    cout << endl;
-
-    cout << "Number of nodes: " << Rcount(first) << endl;
-
-    cout << "Sum of all nodes: " << Rsum(first) << endl;
-
-    cout << "Max number is: " << Max(first) << endl;
-
-    struct Node *temp;
-    temp = LSearch(first, 25);
-    if (temp)
-        cout << "Key is found " << temp->data;
+    // Checking if linked list is sorted or not
+    if (isSorted(first))
+    {
+        cout << "Sorted" << endl;
+    }
     else
-        cout << "Key is not found";
+    {
+        cout << "Not Sorted" << endl;
+    }
+
+    // Removing Duplicate from a linked list
+    RemoveDuplicate(first);
+    display(first);
+
+    Reverse1(first);
+    display(first);
 
     return 0;
 }
