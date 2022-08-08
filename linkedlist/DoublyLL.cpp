707ffd7b5db11ad91c0f175260e9d8d7894a49bd
Aryan Sinha <arison.aryan@gmail.com>
2022-08-09 00:18:49 +0530

    dgdf

--- linkedlist/DoublyLL.cpp
+++ linkedlist/DoublyLL.cpp
@@ -52,10 +52,43 @@ int Length(struct Node *p)
     return len;
 }
 
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+
+    if(index < 0 || index > Length(p))
+        return;
+    if(index==0)
+    {
+        t = new struct Node;
+        t->prev = NULL;
+        t->data = x;
+        t->next = first->next;
+        first = t;
+    }
+    else
+    {
+        t = new struct Node;
+        for(int i=0;i<index-1;i++)
+        {
+            p=p->next;
+        }
+        t->data = x;
+        if(p->next)p->next->prev = t;
+        t->next = p->next;
+        p->next = t;
+        t->prev = p;
+        
+    }
+}
+
 int main()
 {
     int A[] = {10,20,30,40,50};
     create(A,5);
+
+    Insert(first,5,25);
 
     cout << "Length is: " << Length(first) << endl;
 
